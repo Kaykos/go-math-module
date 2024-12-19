bb77f478020db290b494785fbc60df2e81b16d57
Sebastian Bobadilla <bobadillasebas@gmail.com>
2024-12-19 13:14:36 -0500

    New Number interface and made Add generic

--- math.go
+++ math.go
@@ -1,8 +1,14 @@
 // Package go_math_module provides various math operations
 package go_math_module
 
-// Add returns the sum of two integers
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add returns the sum of two numbers
 // https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
