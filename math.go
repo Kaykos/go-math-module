36cb4d28364965de9983314c8a3c4695ec977353
Sebastian Bobadilla <bobadillasebas@gmail.com>
2024-12-19 12:48:14 -0500

    Added godoc comments

--- math.go
+++ math.go
@@ -1,5 +1,8 @@
+// Package go_math_module provides various math operations
 package go_math_module
 
+// Add returns the sum of two integers
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
